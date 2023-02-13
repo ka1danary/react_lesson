fed189e2f5b4111036e3a5a950cbd4c2d78b62a0
ka1danary <99461482+ka1danary@users.noreply.github.com>
2023-02-13 17:51:50 +1000

    bruh

--- src/App.js
+++ src/App.js
@@ -10,16 +10,31 @@ import PostsList from "./components/PostsList";
 import MyButton from "./components/UI/button/MyButton";
 import MyInput from "./components/UI/input/MyInput";
 import PostForm from "./components/PostForm";
+import MySelect from "./components/UI/select/MySelect";
 
 
 function App() {
 
     const [posts, setPosts] = useState ([
-            {id : 1, title : 'JS', body : 'Description'},
-            {id : 2, title : 'JS', body : 'Description'},
-            {id : 3, title : 'JS', body : 'Description'},
+            {id : 1, title : 'aa', body : 'gg'},
+            {id : 2, title : 'bb', body : 'dd'},
+            {id : 3, title : 'hh', body : 'cc'},
 
     ])
+
+    const [selectedSort, setSelectedSort] = useState('')
+    const [searchQuery, setSearchQuery] = useState('')
+
+    function getSortedPosts () {
+        console.log('ОТРАБОАТЛА ФУНКЦИЯ СОРТЕД ПОСТС')
+        if (selectedSort) {
+            return ([...posts].sort((a, b) => a[selectedSort].localeCompare(b[selectedSort]))) // не возвращает сразу отсортированный массив, а мутирует переданный в нее
+        }
+        return posts
+
+    }
+
+    const sortedPost =  getSortedPosts()
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost ])
@@ -29,21 +44,35 @@ function App() {
       setPosts(posts.filter(p => p.id !== post.id))
     }
 
-
-    // Управление данными из управляемого инпута
-
-    const bodyInputRef = useRef()
+    const sortPost = (sort) => {
+        setSelectedSort(sort)
+    }
 
   return (
     <div className="App">
         <Counter/>
         <Input/>
         <PostForm create = {createPost}/>
-
+        <hr style={{margin : '15px 0'}}/>
+        {/*поиск*/}
+        <MyInput
+            placeholder = "Поиск"
+            value={searchQuery}
+            onChange={e => setSearchQuery(e.target.value)}
+        />
+        <MySelect
+            value={selectedSort}
+            onChange={sort => setSelectedSort(sort)} // сразу передаем выбранную пользователем опцию
+            defaultValue="Сортировка по"
+            options={[
+                {value : 'title', name : 'По названиям'},
+                {value : 'body', name : 'По описанию'}
+            ]}
+        />
 
         {posts.length != 0
             ?
-            <PostsList remove={removePost} posts={posts} title="Список постов 1"/>
+            <PostsList remove={removePost} posts={sortedPost} title="Список постов 1"/>
             :
             <h1 style={{textAlign : 'center'}}>
                 Постов нет
